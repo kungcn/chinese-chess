b2bb8610d200d1a1368fc95c38e84f83281ccaeb
chenhch8 <496746441@qq.com>
2017-06-23 21:06:31 +0800

    初步完成个人中心界面

---
+++ web/src/components/pages/UserSetting/children/Content.vue
@@ -0,0 +1,68 @@
+<template>
+<div id="setting-content">
+  <div id="setting-content-container">
+
+  <v-form :items="form1" class="form-item"></v-form>
+  <v-form :items="form2" class="form-item"></v-form>
+
+  </div>
+</div>
+</template>
+
+<script>
+import vForm from './Form'
+
+export default {
+  components: {
+    vForm
+  },
+  props: [ 'level', 'usrAccount' ],
+  data() {
+    return {
+      form1: [
+        {
+          title: '我的等级',
+          text: '九级棋士(150)'
+        },
+        {
+          title: '昵称',
+          text: '叶泽坤'
+        }
+      ],
+      form2: [
+        {
+          title: '性别',
+          text: '男'
+        },
+        {
+          title: '地区',
+          text: '广东省'
+        },
+        {
+          title: '个性签名',
+          text: '这人很懒，...'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+#setting-content {
+  position: relative;
+  width: 100%;
+  height: 82vh;
+}
+#setting-content-container {
+  position: relative;
+  width: 78%;
+  height: 100%;
+  margin: 0 auto;
+  padding: 4%;
+}
+.form-item {
+  margin: 5vh 0;
+}
+
+</style>
---
+++ web/src/components/pages/UserSetting/children/Form.vue
@@ -0,0 +1,92 @@
+<template>
+<div class="setting-form">
+  <div class="setting-border border-horizontal"></div>
+  <div class="setting-border border-horizontal"></div>
+  <div class="setting-border border-vertical"></div>
+  <div class="setting-border border-vertical"></div>
+  <div class="setting-item" v-for="(item, index) in items">
+    <p>{{ item.title }}</p>
+    <img src="../../../../assets/pages/UserSetting/btn_r.png" />
+    <span>{{ item.text }}</span>
+    <div v-if="index < (items.length - 1)"></div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  props: [ 'items' ]
+}
+</script>
+
+<style>
+.setting-form {
+  position: relative;
+  width: 100%;
+  border: 0;
+  height: auto
+}
+.setting-border {
+  position: absolute;
+  border: 1px solid #8a683a;
+}
+.border-horizontal {
+  width: 1vw;
+  height: 100%;
+}
+.border-horizontal:nth-child(1) {
+  left: -1vw;
+  border-right: 0;
+}
+.border-horizontal:nth-child(2) {
+  right: -1vw;
+  border-left: 0;
+}
+.border-vertical {
+  width: 100%;
+  height: 1vw;
+}
+.border-vertical:nth-child(3) {
+  top: -1vw;
+  border-bottom: 0;
+}
+.border-vertical:nth-child(4) {
+  bottom: -1vw;
+  border-top: 0;
+}
+.setting-item {
+  position: relative;
+  width: 100%;
+  height: 8vw;
+  line-height: 8vw;
+  padding: 0 3%;
+  font-size: 5vw;
+  cursor: pointer;
+}
+.setting-item > p {
+  margin: 0;
+  float: left;
+  text-align: center;
+  color: #8b6d63
+}
+.setting-item > img {
+  float: right;
+  width: 5%;
+  height: 80%;
+  //top: 50%;
+  transform: translate(0, 12%);
+  margin-left: 2%;
+}
+.setting-item > span {
+  float: right;
+  color: #5d5749;
+  font-size: 4.5vw;
+}
+.setting-item > div {
+  width: calc(100% + 6vw);
+  height: 100%;
+  position: relative;
+  left: -3vw;
+  border-bottom: 1px solid #927c74;
+}
+</style>
