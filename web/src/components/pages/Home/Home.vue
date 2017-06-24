f9ee345aba5aad2e3b01083b64b7559bd3a2b834
boyan12138 <1226741211@qq.com>
2017-06-24 22:14:27 +0800

    edit software design document

--- web/src/components/pages/Home/Home.vue
+++ web/src/components/pages/Home/Home.vue
@@ -9,14 +9,14 @@
     <form id="f_login" name="f_login" method="post" class="form">
       <mu-text-field label="username" hintText='username'/><br/>
       <mu-text-field label="password" hintText='password' type="password" labelFloat/><br/>
-      <mu-raised-button type='submit' label="登录游戏中心" class="demo-flat-button" secondary/>
+      <mu-raised-button @click="login()" type='submit' label="登录游戏中心" class="demo-flat-button" secondary/>
     </form>
   </div>
   <div v-if="activeTab === 'register'">
     <form id="f_register" name="f_register" method="post" class="form">
       <mu-text-field label="username" hintText='用户名不能以数字或下划线开头'/><br/>
       <mu-text-field label="password" hintText='密码长度为6-16个字符' type="password" labelFloat/><br/>
-      <mu-raised-button type='submit' label="确认注册并登录" class="demo-flat-button" secondary/>
+      <mu-raised-button type='submit' label="确认注册并登录" class="demo-flat-button" @click="register()" secondary/>
     </form>
   </div>
   <div v-if="activeTab === 'guest'">
--- web/src/router/index.js
+++ web/src/router/index.js
@@ -9,7 +9,7 @@ Vue.use(Router)
 export default new Router({
     routes: [{
         path: '/',
-        name: 'Home',
-        component: Home
+        name: 'Grade',
+        component: Grade
     }]
 })
