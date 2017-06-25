fee4ed752aab4562d18e16897e86e544e324bdb5
kungcn <kcnnow@gmail.com>
2017-06-25 18:08:40 +0800

    router file update

---
+++ web/src/components/kungcn/chess.vue
@@ -0,0 +1,13 @@
+<template>
+    <p>
+        这个是棋盘哦
+    </p>
+</template>
+
+<script>
+    
+</script>
+
+<style scoped>
+    
+</style>
--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,19 +1,26 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import UserSetting from '@/components/chenhch8/pages/UserSetting/UserSetting'
-import Grade from '@/components/chenhch8/pages/Grade/Grade'
+import Chess from '@/components/kungcn/chess'
+
+// using async to load components expect for the first showing one
+// import UserSetting from '@/components/chenhch8/pages/UserSetting/UserSetting'
+// import Grade from '@/components/chenhch8/pages/Grade/Grade'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [{
     path: '/',
+    name: 'Chess',
+    component: Chess
+  }, {
+    path: '/setting',
     name: 'UserSetting',
-    component: UserSetting
+    component: resolve => require(['@/components/chenhch8/pages/UserSetting/UserSetting'], resolve)
   }, {
     path: '/grade',
     name: 'Grade',
-    component: Grade
+    component: resolve => require(['@/components/chenhch8/pages/Grade/Grade'], resolve)
   }, {
     path: '*',
     redirect: '/'
